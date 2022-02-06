7a09c283b040c600b0c2e0f23d7e3bdb3db9c5d1
James Reilly <jreilly2112@gmail.com>
2022-02-06 14:28:34 -0500

    starting to create basic html structure

---
+++ Assets/JS/script.js
@@ -0,0 +1,69 @@
+var searchForm = document.querySelector("#search-form");
+
+var searchButton = document.querySelector("#search-button");
+
+var resetButton = document.querySelector("#reset-button");
+
+var searchField = document.querySelector("#search-field");
+
+var formList = document.querySelector("#localList");
+
+var APIID = "Weather Dashboard";
+
+var API = "3edcfa6c11db04ad96a6eaf0dac6ed19"
+
+var city;
+
+var apiURL;
+
+var cityList = [];
+
+$(document).ready(function()
+{
+    var appendCity = JSON.parse(localStorage.getItem('cities'))
+
+    for(const el of appendCity)
+    {
+        console.log(el);
+        var item = document.createElement('li');
+
+        item.textContent = el;
+
+        formList.appendChild(item);
+    }
+})
+
+resetButton.addEventListener('click', function()
+{
+    localStorage.clear();
+
+    while (formList.firstChild) {
+        formList.removeChild(formList.firstChild);
+    }
+})
+
+searchForm.addEventListener('submit', function(event)
+{
+    event.preventDefault();
+    city = searchField.value;
+    apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&id=${APIID}&APPID=${API}`;
+    console.log(apiURL);
+    fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+
+    cityList.push(city);
+
+    var item = document.createElement('li');
+
+    item.textContent = city;
+
+    formList.appendChild(item);
+
+    localStorage.setItem('cities', JSON.stringify(cityList));
+}
+)
